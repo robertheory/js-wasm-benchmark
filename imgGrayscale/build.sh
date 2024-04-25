685fff19a0530171f4ec6d670558ba186659bfbf
Roberto Costa <roberto.araujo339@gmail.com>
2024-04-25 17:37:53 -0300

    removed other cases

--- imgGrayscale/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-emcc -O3 -s EXPORTED_RUNTIME_METHODS='["cwrap"]' \
-  wasm-grayscale.c \
-  -Os \
-  -s WASM=1 \
-  -s "EXPORTED_FUNCTIONS=['_wasmGrayscale']" \
-  -o wasm-grayscale.js
